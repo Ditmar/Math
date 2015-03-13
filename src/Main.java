e1cdc5efcc6399af5f2201c8524c8dcf720f960e
ditmar <ditmarcastro@gmail.com>
2015-03-13 11:17:59 -0300

    Otra version

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA MUNDO");
-		Mate p=new Mate();
-		int r=p.producto(12, 14);
-		System.out.println(r+"");
+		
 	}
 
 }
