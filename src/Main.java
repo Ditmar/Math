46d13c67f60587a15b6ad6cd349118c726a08924
ditmar <ditmarcastro@gmail.com>
2015-03-13 12:08:30 -0300

    multiplicacion matricial

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("HOLA MUNDO");
-		
+		Mate p=new Mate();
+		int r=p.producto(12, 14);
+		int[][] a={{1,2,3},{4,5,6},{7,8,9}};
+		int [][] b={{1,2,3},{4,5,6},{7,8,9}};
+		int [][] resultado=p.multiplicacionmatricial(a,b);
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=0;j<a[0].length;j++){
+				System.out.print(""+resultado[i][j]+" ");
+			}
+			System.out.println("");
+		}
 	}
 
 }
--- src/Mate.java
+++ src/Mate.java
@@ -15,4 +15,22 @@ public class Mate {
 	{
 		return 1;
 	}
+	public int[][] multiplicacionmatricial(int a[][],int b[][])
+	{
+		int[][] r=new int[10][10];
+		int suma;
+		for(int i =0;i<a.length;i++)
+		{
+			for(int j=0;j<a[0].length;j++)
+			{
+				suma=0;
+				for(int k=0;k<a.length;k++)
+				{
+					suma+=a[i][k]*b[k][j];
+				}
+				r[i][j]=suma;
+			}
+		}
+		return r;
+	}
 }
